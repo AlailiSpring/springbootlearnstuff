777d74217c8ec4de2281cd58513486e4676c3c0b
AlailiSpring <libdqd@126.com>
2020-12-22 23:23:05 +0800

    前后端分离整合在一起： 1、加入thyemleaf视图解析器 2、修改RestController为Controller，方便自由的控制请求的返回类型

--- src/main/java/com/alaili/springbootlearnstuff/controller/ProductController.java
+++ src/main/java/com/alaili/springbootlearnstuff/controller/ProductController.java
@@ -3,22 +3,32 @@ package com.alaili.springbootlearnstuff.controller;
 import com.alaili.springbootlearnstuff.service.ProductService;
 import com.alaili.springbootlearnstuff.vo.DataVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @Description:
  * @Author: LiBaoDeng
  * @Date: 2020-12-16 23:59
  */
-@RestController
+
+@Controller
 public class ProductController {
 
     @Autowired
     ProductService productService;
 
     @RequestMapping("/list")
+    @ResponseBody
     public DataVO list(Integer page,Integer limit) {
         return productService.selectAllData(page,limit);
+    }
+
+    @GetMapping("/{url}")
+    public String redirect(@PathVariable("url") String url) {
+        return url;
     }
 }
